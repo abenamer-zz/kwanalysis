76effe676c4ea632ea99211fdabf791ccb659b6c
Tom McCracken <tomdude48@258411.no-reply.drupal.org>
2010-05-24 22:39:57 +0000

    Initial commit of keywordanalysis module. This module analyzes content and extracts keywords

---
+++ kwanalysis.js
@@ -0,0 +1,10 @@
+// $Id$
+/*
+ * Implementation of hook_contentanalysis_data()
+ * Gets the data from the custom fields to attach to the AJAX post data.
+ */ 
+var contentanalysisexample_contentanalysis_data = function() {		
+  data = new Array();	
+  data['name'] = document.getElementById('edit-contentanalysisexample-name').value;	
+  return data;
+}
