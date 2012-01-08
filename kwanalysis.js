7a36418102840ee6d8e1638b6731996d08b43943
Tom McCracken <tomm@getlevelten.com>
2012-01-08 10:28:56 -0600

    intial port to D7

--- kwanalysis.js
+++
@@ -1,9 +0,0 @@
-/*
- * Implementation of hook_contentanalysis_data()
- * Gets the data from the custom fields to attach to the AJAX post data.
- */ 
-var contentanalysisexample_contentanalysis_data = function() {		
-  data = new Array();	
-  data['name'] = document.getElementById('edit-contentanalysisexample-name').value;	
-  return data;
-}
